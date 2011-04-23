c7d891bfae8a70a1dc18e1cc6d5dcfa7b5e4dd46
ipocepan <ipocepan@gmail.com>
2011-04-23 15:20:37 +0200

    Popravljen bug kod ucitavanja matrice

--- qLearning.py
+++ qLearning.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python
 
 import random
+import sys
 
 def dobri(red):
   ret = []
@@ -10,33 +11,55 @@ def dobri(red):
   return ret
 
 def main():
-  f = open('R.mat', 'rU')
-  cijeliFajl = f.read()
-  R = [map(lambda x: float(x), s.split(',')) for s in cijeliFajl.split('\n')]
-  #print R
   
-  q=[]
+  if len(sys.argv) != 4:
+    print sys.argv[0], 'filename', 'pocetak', 'kraj'
+    sys.exit(1)
+  
+  filename = sys.argv[1]
+  poc = int(sys.argv[2])
+  kraj = int(sys.argv[3])
+  
+  # ucitavam matricu iz datoteke
+  f = open(filename, 'rU')
+  cijeliFajl = f.read()
+  R = [map(lambda x: float(x), s.split(',')) for s in cijeliFajl.split()]
+  
+  # inicijaliziram Q na 0
+  Q=[]
   for red in R:
     qr=[]
     for broj in red:
       qr.append(0.0)
-    q.append(qr)
+    Q.append(qr)
 
+  #inicijaliziram gammu
   gamma = 0.8
+  
+  # Q learning
   for i in range(1000):
-    s = random.randint(0, 5)
+    s = random.randint(0, kraj)
     while True:
         r = random.choice(dobri(R[s]))
-        q[s][r] = R[s][r] + gamma * max(q[r])
-        if s == 5: break
+        Q[s][r] = R[s][r] + gamma * max(Q[r])
+        if s == kraj: break
         s = r
-  for red in q:
+  
+  # ispis matrice Q      
+  for red in Q:
     print red
-    
-  s = 2
-  while s != 5:
-    s = q[s].index(max(q[s]))
-    print s
+  
+  # racuna najbolji put
+  put = []  
+  s = poc
+  while s != kraj:
+    s = Q[s].index(max(Q[s]))
+    put.append(s)
+  
+  # ispis puta
+  print
+  print 'Najkraci put'
+  print put
   
 if __name__=='__main__':
   main()
