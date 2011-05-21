cbbecdc9ca9bba279f4f5f9e09f02703ccdb61a4
dperetin <dperetin@gmail.com>
2011-05-21 14:03:49 +0200

    Dodane neke slike u prezentaciju, jos nema teksta

--- generator.py
+++ generator.py
@@ -22,8 +22,8 @@ def main():
         matrica.append(redak)
      
     broj_elementa = random.sample(range(1, 3), 1);
-    puni_elementi = random.sample(range(1, 3), broj_elementa[0])
-    #puni_elementi = random.sample(range(size), broj_elementa[0])
+    #puni_elementi = random.sample(range(1, 3), broj_elementa[0])
+    puni_elementi = random.sample(range(size), broj_elementa[0])
     for j in puni_elementi:
             matrica[0][j] = 0
             matrica[j][0] = matrica[0][j]
@@ -31,8 +31,8 @@ def main():
     for  i in range(1, size):
         #print i
         broj_elementa = random.sample(range(1, 3), 1);
-        puni_elementi = random.sample(range(i + 1, min(i+3, size)), min(broj_elementa[0], size - i - 1))
-        #puni_elementi = random.sample(range(i + 1), min(broj_elementa[0], size - i - 1))
+        #puni_elementi = random.sample(range(i + 1, min(i+3, size)), min(broj_elementa[0], size - i - 1))
+        puni_elementi = random.sample(range(i + 1), min(broj_elementa[0], size - i - 1))
         elementi_prije = random.sample(range(max(i-2, 0), i), 1)
         for j in elementi_prije:
             matrica[i][j] = 0
